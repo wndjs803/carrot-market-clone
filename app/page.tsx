5d508a07802ecc55de013b4488d9d92c5d47faca
김주언 <wndjs803@naver.com>
2024-11-18 22:00:19 +0900

    tailwind variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,19 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-500">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1">
-              In transit
-            </span>
-            <span className="text-4xl font-semibold">Coolblue</span>
-          </div>
-          <div className="size-12 rounded-full bg-orange-400" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span
-            className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full
-          hover:bg-green-500 hover:scale-125 transition"
-          >
-            Today
-          </span>
-          <span>9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 w-full h-2 absolute rounded-full" />
-          <div className="bg-green-400 w-2/3 h-2 absolute rounded-full" />
-        </div>
-        <div className="flex justify-between mt-5 text-gray-600">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400">Delivered</span>
-        </div>
+      <div
+        className="bg-white w-full shadow-lg p-5 rounded-3xl 
+      max-w-screen-sm flex flex-col gap-2"
+      >
+        <input
+          className="w-full rounded-full h-10 bg-gray-200 pl-5 
+          outline-none ring ring-transparent placeholder:drop-shadow"
+          type="text"
+          placeholder="Search here..."
+        />
+        <button className="bg-black bg-opacity-85 text-white rounded-full focus:scale-90 active:scale-90 transition-transform font-medium outline-none">
+          Search
+        </button>
       </div>
     </main>
   );
