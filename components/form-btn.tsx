37fed66cc41b3838d11c55ee7dfa53f6070a43fd
김주언 <wndjs803@naver.com>
2024-11-26 17:19:28 +0900

    useFormStatus

--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,14 +1,19 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
 interface FormButtonProps {
-  loading: boolean;
   text: string;
 }
-export default function FormButton({ loading, text }: FormButtonProps) {
+
+export default function FormButton({ text }: FormButtonProps) {
+  const { pending } = useFormStatus(); // 무조건 form tag의 자식에서만 사용가능
   return (
     <button
-      disabled={loading}
+      disabled={pending}
       className="primary-btn h-10 disabled:bg-neutral-400  disabled:text-neutral-300 disabled:cursor-not-allowed"
     >
-      {loading ? "로딩 중" : text}
+      {pending ? "로딩 중" : text}
     </button>
   );
 }
