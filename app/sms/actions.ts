bcf249315927681e1084d300f64f92aadae76204
김주언 <wndjs803@naver.com>
2024-11-28 01:08:51 +0900

    coerce

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,3 +1,10 @@
 "use server";
 
-export async function smsVerification(prevState: any, formData: FormData) {}
+import validator from "validator";
+import { z } from "zod";
+
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+export async function smsLogin(prevState: any, formData: FormData) {}
