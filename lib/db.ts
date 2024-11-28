0bafa4e66b99283dd27c3db5bc59847c03e1c92d
김주언 <wndjs803@naver.com>
2024-11-28 23:53:09 +0900

    prisma client

---
+++ lib/db.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function test() {
+  const user = await db.user.findMany({
+    where: {
+      username: {
+        contains: "es",
+      },
+    },
+  });
+  console.log(user);
+}
+
+test();
+
+export default db;
