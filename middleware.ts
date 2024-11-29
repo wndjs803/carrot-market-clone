ab6507a3c0912bab012376fd4568cbbd7de3bd57
김주언 <wndjs803@naver.com>
2024-11-29 22:13:24 +0900

    Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Route {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Route = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  if (pathname === "/") {
-    const response = NextResponse.next();
-    response.cookies.set("middleware-cookie", "hello");
-    return response;
-  }
-  if (pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    // 로그인 하지 않은 유저가
+    if (!exists) {
+      // 허락되지 않은 url 접근 시
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
   }
 }
 
