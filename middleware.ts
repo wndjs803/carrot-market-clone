f6bb7a6cf43ddb856ad84d2c5c1544c08c30e155
김주언 <wndjs803@naver.com>
2024-11-29 21:57:08 +0900

    matcher

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,17 @@
-import { NextRequest } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  if (request.nextUrl.pathname === "/profile") {
+  const pathname = request.nextUrl.pathname;
+  if (pathname === "/") {
+    const response = NextResponse.next();
+    response.cookies.set("middleware-cookie", "hello");
+    return response;
+  }
+  if (pathname === "/profile") {
     return Response.redirect(new URL("/", request.url));
   }
 }
+
+export const config = {
+  matcher: ["/", "/profile", "/create-account"],
+};
