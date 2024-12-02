c7fc4bf5c9a3aa09510df1c2d77b486c4a5a28cd
김주언 <wndjs803@naver.com>
2024-12-02 23:31:56 +0900

    image hostnames

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
