521df60572f957023b4ea8d8a2ff8e60ac1ce8fa
김주언 <wndjs803@naver.com>
2024-12-02 17:27:06 +0900

    skeletons

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,4 +1,13 @@
-export default function Proudct() {
+import db from "@/lib/db";
+import Loading from "./loading";
+
+async function getProducts() {
+  //   db.product.findMany();
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+}
+
+export default async function Proudct() {
+  const proudct = await getProducts();
   return (
     <div>
       <h1 className="text-white text-4xl">Product!</h1>
