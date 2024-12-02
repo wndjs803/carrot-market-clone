14c47954c3b97e2caa7dbe7d4008bdf666e4cdf6
김주언 <wndjs803@naver.com>
2024-12-03 00:24:06 +0900

    pagination

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -12,24 +12,35 @@ interface ProductListProps {
 export default function ProductList({ initialProducts }: ProductListProps) {
   const [products, setProducts] = useState(initialProducts);
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastpage, setIsLastPage] = useState(false);
   const onLoadMoreClick = async () => {
     setIsLoading(true);
-    const newProducts = await getMoreProducts(1);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
     setIsLoading(false);
-    setProducts((prev) => [...prev, ...newProducts]);
   };
   return (
     <div className="p-5 flex flex-col gap-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        onClick={onLoadMoreClick}
-        disabled={isLoading}
-        className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-      >
-        {isLoading ? "로딩 중" : "Load More"}
-      </button>
+      {isLastpage ? (
+        "No More Items"
+      ) : (
+        <button
+          onClick={onLoadMoreClick}
+          disabled={isLoading}
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩 중" : "Load More"}
+        </button>
+      )}
     </div>
   );
 }
