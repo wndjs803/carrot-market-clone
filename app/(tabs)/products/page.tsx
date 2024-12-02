1d9aca31bb923bd1076a6a8f6d9b6d061f1e5c1b
김주언 <wndjs803@naver.com>
2024-12-02 21:56:18 +0900

    product component

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,16 +1,27 @@
+import ListProduct from "@/components/list-product";
 import db from "@/lib/db";
-import Loading from "./loading";
 
 async function getProducts() {
-  //   db.product.findMany();
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const products = db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+
+  return products;
 }
 
 export default async function Proudct() {
-  const proudct = await getProducts();
+  const proudcts = await getProducts();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Product!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {proudcts.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
