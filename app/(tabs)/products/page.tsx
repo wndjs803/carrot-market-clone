5ed4eb8e123eadf126d1fbae328d06ca7089d2d5
김주언 <wndjs803@naver.com>
2024-12-02 16:59:46 +0900

    tab bar

---
+++ app/(tabs)/products/page.tsx
@@ -0,0 +1,7 @@
+export default function Proudct() {
+  return (
+    <div>
+      <h1 className="text-white text-4xl">Product!</h1>
+    </div>
+  );
+}
---
+++ components/tab-bar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import {
+  NewspaperIcon as OutlineNewspaperIcon,
+  HomeIcon as OutlineHomeIcon,
+  ChatBubbleOvalLeftEllipsisIcon as OutlineChatIcon,
+  VideoCameraIcon as OutlineLiveIcon,
+  UserIcon as OutlineUserIcon,
+} from "@heroicons/react/24/outline";
+import {
+  NewspaperIcon as SolidNewspaperIcon,
+  HomeIcon as SolidHomeIcon,
+  ChatBubbleOvalLeftEllipsisIcon as SolidChatIcon,
+  VideoCameraIcon as SolidLiveIcon,
+  UserIcon as SolidUserIcon,
+} from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function TabBar() {
+  const pathname = usePathname();
+  return (
+    <div
+      className="fixed bottom-0 w-full mx-auto max-w-screen-md grid 
+    grid-cols-5 border-neutral-600 border-t px-5 py-3 *:text-white"
+    >
+      <Link href="/products" className="flex flex-col items-center gap-px">
+        {pathname === "/products" ? (
+          <SolidHomeIcon className="w-7 h-7" />
+        ) : (
+          <OutlineHomeIcon className="w-7 h-7" />
+        )}
+        <span>홈</span>
+      </Link>
+      <Link href="/life" className="flex flex-col items-center gap-px">
+        {pathname === "/life" ? (
+          <SolidNewspaperIcon className="w-7 h-7" />
+        ) : (
+          <OutlineNewspaperIcon className="w-7 h-7" />
+        )}
+        <span>동네생활</span>
+      </Link>
+      <Link href="/chat" className="flex flex-col items-center gap-px">
+        {pathname === "/chat" ? (
+          <SolidChatIcon className="w-7 h-7" />
+        ) : (
+          <OutlineChatIcon className="w-7 h-7" />
+        )}
+        <span>채팅</span>
+      </Link>
+      <Link href="/live" className="flex flex-col items-center gap-px">
+        {pathname === "/live" ? (
+          <SolidLiveIcon className="w-7 h-7" />
+        ) : (
+          <OutlineLiveIcon className="w-7 h-7" />
+        )}
+        <span>쇼핑</span>
+      </Link>
+      <Link href="/profile" className="flex flex-col items-center gap-px">
+        {pathname === "/profile" ? (
+          <SolidUserIcon className="w-7 h-7" />
+        ) : (
+          <OutlineUserIcon className="w-7 h-7" />
+        )}
+        <span>나의 당근</span>
+      </Link>
+    </div>
+  );
+}
