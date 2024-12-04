5828adc4956103e33e9f0eaae4c09da41985df99
김주언 <wndjs803@naver.com>
2024-12-04 22:54:20 +0900

    product upload

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -4,6 +4,7 @@ import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
@@ -18,9 +19,10 @@ export default function AddProduct() {
     const url = URL.createObjectURL(file);
     setPreview(url);
   };
+  const [state, action] = useFormState(uploadProduct, null);
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={action} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex justify-center items-center flex-col
@@ -35,18 +37,32 @@ export default function AddProduct() {
               <PhotoIcon className="w-20" />
               <div className="text-neural-400 text-sm">
                 사진을 추가해주세요.
+                {state?.fieldErrors.photo}
               </div>
             </>
           ) : null}
         </label>
         <input onChange={onImageChange} type="file" id="photo" name="photo" />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" required placeholder="가격" type="number" />
+        <Input
+          name="title"
+          required
+          placeholder="제목"
+          type="text"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          required
+          placeholder="가격"
+          type="number"
+          errors={state?.fieldErrors.price}
+        />
         <Input
           name="description"
           required
           placeholder="자세한 설명"
           type="text"
+          errors={state?.fieldErrors.description}
         />
         <Button text="작성완료" />
       </form>
