002de8d71ecba5af74b9df331bf05cd7d6cab5b3
김주언 <wndjs803@naver.com>
2024-12-04 22:21:01 +0900

    form action

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -3,20 +3,41 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { uploadProduct } from "./actions";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
-  const onImageChange = () => {};
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) {
+      return;
+    }
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
   return (
     <div>
-      <form className="p-5 flex flex-col gap-5">
+      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex justify-center items-center flex-col
-        text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer"
+        text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer
+        bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-neural-400 text-sm">사진을 추가해주세요.</div>
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neural-400 text-sm">
+                사진을 추가해주세요.
+              </div>
+            </>
+          ) : null}
         </label>
         <input onChange={onImageChange} type="file" id="photo" name="photo" />
         <Input name="title" required placeholder="제목" type="text" />
