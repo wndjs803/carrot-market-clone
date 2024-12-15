0285175534b23d3f5abcb65a9b679d3fc8e5106d
김주언 <wndjs803@naver.com>
2024-12-15 21:03:46 +0900

    revalidateTag

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,8 +5,18 @@ import { UserIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { unstable_cache as nextCache, revalidateTag } from "next/cache";
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) {
+    return session.id === userId;
+  }
+  return false;
+}
 
 async function getProduct(id: number) {
+  console.log("product");
   const product = await db.product.findUnique({
     where: {
       id,
@@ -23,12 +33,32 @@ async function getProduct(id: number) {
   return product;
 }
 
-async function getIsOwner(userId: number) {
-  const session = await getSession();
-  if (session.id) {
-    return session.id === userId;
-  }
-  return false;
+const getCachedProduct = nextCache(getProduct, ["product-detail"], {
+  tags: ["product-detail", "xxxx"],
+});
+
+async function getProductTitle(id: number) {
+  console.log("title");
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      title: true,
+    },
+  });
+  return product;
+}
+
+const getCachedProductTitle = nextCache(getProductTitle, ["product-title"], {
+  tags: ["product-title", "xxxx"],
+});
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const product = await getCachedProductTitle(Number(params.id));
+  return {
+    title: product?.title,
+  };
 }
 
 export default async function ProductDetail({
@@ -40,21 +70,22 @@ export default async function ProductDetail({
   if (isNaN(id)) {
     return notFound();
   }
-
-  const product = await getProduct(id);
+  const product = await getCachedProduct(id);
   if (!product) {
     return notFound();
   }
-
   const isOwner = await getIsOwner(product.userId);
-
+  const revalidate = async () => {
+    "use server";
+    revalidateTag("xxxx");
+  };
   return (
-    <div className="h-[120vh]">
+    <div className="pb-40">
       <div className="relative aspect-square">
         <Image
           className="object-cover"
           fill
-          src={product.photo}
+          src={`${product.photo}`}
           alt={product.title}
         />
       </div>
@@ -62,9 +93,9 @@ export default async function ProductDetail({
         <div className="size-10 overflow-hidden rounded-full">
           {product.user.avatar !== null ? (
             <Image
+              src={product.user.avatar}
               width={40}
               height={40}
-              src={product.user.avatar}
               alt={product.user.username}
             />
           ) : (
@@ -79,22 +110,23 @@ export default async function ProductDetail({
         <h1 className="text-2xl font-semibold">{product.title}</h1>
         <p>{product.description}</p>
       </div>
-      <div className="fixed w-full bottom-0 left-0 p-5 bg-neutral-800 flex justify-between items-center">
-        <span className="font-semibold text-lg">
-          {formatToWon(product.price)}
+      <div className="fixed w-full bottom-0  p-5 pb-10 bg-neutral-800 flex justify-between items-center max-w-screen-sm">
+        <span className="font-semibold text-xl">
+          {formatToWon(product.price)}원
         </span>
         {isOwner ? (
-          <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
-            Delete ListProduct
-          </button>
-        ) : (
-          <Link
-            className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
-            href={``}
-          >
-            채팅하기
-          </Link>
-        )}
+          <form action={revalidate}>
+            <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
+              Revalidate title cache
+            </button>
+          </form>
+        ) : null}
+        <Link
+          className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
+          href={``}
+        >
+          채팅하기
+        </Link>
       </div>
     </div>
   );
